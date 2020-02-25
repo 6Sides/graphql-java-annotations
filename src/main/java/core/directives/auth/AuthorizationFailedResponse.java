780fe2a71c1e6407fde915da756f9b449601470d
jbeck18 <j.beck18@me.com>
2020-02-25 14:38:38 -0500

    DateTime now uses OffsetDateTime

--- src/main/java/core/directives/auth/AuthorizationFailedResponse.java
+++
@@ -1,9 +0,0 @@
-package core.directives.auth;
-
-public class AuthorizationFailedResponse {
-
-    private static String type = "Authorization Error";
-    private static String title = "You are not authorized to access this resource";
-    private static String message = "Your account does not have access to this resource. Contact your system administrator if you believe this is a mistake.";
-
-}
--- src/main/java/core/scalars/DateTime.java
+++ src/main/java/core/scalars/DateTime.java
@@ -1,18 +1,22 @@
 package core.scalars;
 
-import graphql.language.IntValue;
+import graphql.language.StringValue;
 import graphql.schema.Coercing;
 import graphql.schema.CoercingParseLiteralException;
 import graphql.schema.CoercingParseValueException;
 import graphql.schema.CoercingSerializeException;
 import graphql.schema.GraphQLScalarType;
-import java.math.BigInteger;
-import java.time.Instant;
+import java.text.SimpleDateFormat;
+import java.time.OffsetDateTime;
+import java.time.format.DateTimeParseException;
 import schemabuilder.annotations.graphql.GraphQLScalar;
 import schemabuilder.annotations.graphql.GraphQLSchemaConfiguration;
 
 @GraphQLSchemaConfiguration
 public class DateTime {
+
+    // Format in ISO 8601
+    private static SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX");
 
     @GraphQLScalar("DateTime")
     public static GraphQLScalarType getDateTimeScalar() {
@@ -24,22 +28,23 @@ public class DateTime {
                             @Override
                             public Object serialize(Object dataFetcherResult)
                                     throws CoercingSerializeException {
-                                if (!(dataFetcherResult instanceof Number)) {
+                                if (!(dataFetcherResult instanceof OffsetDateTime)) {
                                     throw new CoercingSerializeException(
                                             "Unable to serialize "
                                                     + dataFetcherResult
                                                     + " as a DateTime");
                                 }
-                                return Instant.ofEpochSecond((Long) dataFetcherResult).toString();
+
+                                return formatter.format(dataFetcherResult);
                             }
 
                             @Override
                             public Object parseValue(Object input)
                                     throws CoercingParseValueException {
-                                Long possibleDateTime = (Long) input;
+                                String possibleDateTime = (String) input;
                                 try {
-                                    return Instant.ofEpochSecond(possibleDateTime);
-                                } catch (IllegalArgumentException ex) {
+                                    return OffsetDateTime.parse(possibleDateTime);
+                                } catch (DateTimeParseException ex) {
                                     throw new CoercingParseValueException(ex.getMessage());
                                 }
                             }
@@ -47,15 +52,12 @@ public class DateTime {
                             @Override
                             public Object parseLiteral(Object input)
                                     throws CoercingParseLiteralException {
-                                if (input instanceof IntValue) {
-                                    BigInteger possibleDateTime = ((IntValue) input).getValue();
+                                if (input instanceof StringValue) {
+                                    String possibleDateTime = ((StringValue) input).getValue();
 
                                     try {
-                                        Instant result =
-                                                Instant.ofEpochSecond(
-                                                        possibleDateTime.longValueExact());
-                                        return result;
-                                    } catch (IllegalArgumentException ex) {
+                                        return OffsetDateTime.parse(possibleDateTime);
+                                    } catch (DateTimeParseException ex) {
                                         throw new CoercingParseLiteralException(ex.getMessage());
                                     }
                                 }
