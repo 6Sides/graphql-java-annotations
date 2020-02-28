2019058b45892487f7416479312875d9f26574b2
jbeck18 <j.beck18@me.com>
2020-02-28 17:20:20 -0500

    Can now access package private classes

--- src/main/java/schemabuilder/processor/wiring/DefaultInstanceFetcher.java
+++ src/main/java/schemabuilder/processor/wiring/DefaultInstanceFetcher.java
@@ -1,13 +1,18 @@
 package schemabuilder.processor.wiring;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
 public class DefaultInstanceFetcher implements InstanceFetcher {
 
     // TODO: Take appropriate action based on exception thrown
     @Override
     public Object getInstance(Class<?> clazz) {
         try {
-            return clazz.newInstance();
-        } catch(IllegalAccessException | InstantiationException e) {
+            Constructor<?> constructor = clazz.getDeclaredConstructor();
+            constructor.setAccessible(true);
+            return constructor.newInstance();
+        } catch(IllegalAccessException | InstantiationException | NoSuchMethodException | InvocationTargetException e) {
             e.printStackTrace();
         }
         return null;
