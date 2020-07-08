85c70d25f99071e7e11f021e93f8de6578f1d62b
jbeck18 <j.beck18@me.com>
2020-07-08 14:52:36 -0400

    fixed but with new annotations

--- src/main/java/schemabuilder/annotations/graphql/federation/GraphQLType.kt
+++ src/main/java/schemabuilder/annotations/graphql/federation/GraphQLType.kt
@@ -2,7 +2,7 @@ package schemabuilder.annotations.graphql.federation
 
 import kotlin.reflect.KClass
 
-@Target(AnnotationTarget.TYPE)
+@Target(AnnotationTarget.TYPE, AnnotationTarget.CLASS)
 @kotlin.annotation.Retention(AnnotationRetention.RUNTIME)
 annotation class GraphQLType(
     val name: String,
